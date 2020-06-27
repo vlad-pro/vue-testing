d4ebd397c82ea0a4ad874fd199e80226e94e84cb
vproskurin <vladimir.proskurin@gmail.com>
2020-06-26 23:46:35 -0700

    testing api calls. with mocking

--- tests/unit/MessageDisplay.spec.js
+++ tests/unit/MessageDisplay.spec.js
@@ -4,16 +4,31 @@ import { getMessage } from "@/services/axios";
 import flushPromises from "flush-promises";
 
 jest.mock("@/services/axios");
+beforeEach(() => {
+  jest.clearAllMocks();
+});
 
 describe("MessageDisplay", () => {
   test("Calls getMessage and displays message", async () => {
     const MockedMessage = "Mock message from DB";
     getMessage.mockResolvedValueOnce({ text: MockedMessage });
     const wrapper = mount(MessageDisplay);
-    await flushPromises()
+    await flushPromises();
 
-    
+    expect(getMessage).toHaveBeenCalledTimes(1);
+    const message = wrapper.find('[data-testid="message"]').element.textContent;
+    expect(message).toEqual(MockedMessage);
   });
 
-  test("Displays an error when getMessage call fails", async () => {});
+  test("Displays an error when getMessage call fails", async () => {
+    const MockError = "Oops! Something went wrong.";
+    getMessage.mockRejectedValueOnce({ text: MockError });
+    const wrapper = mount(MessageDisplay);
+
+    await flushPromises();
+    expect(getMessage).toHaveBeenCalledTimes(1);
+    const error = wrapper.find('[data-testid="message-error"]').element
+      .textContent;
+    expect(error).toEqual(MockError);
+  });
 });
